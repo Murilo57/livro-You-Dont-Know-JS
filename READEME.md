ef8d34c61fe7c883081550d5fd3de2640dedf7db
Murilo <murilo.ti32@outlook.com>
2023-12-27 17:13:39 -0300

    Ex funções parte 1 e 2(incompleto)

---
+++ READEME.md
@@ -0,0 +1,7 @@
+# Livro - You Know Dont JavaScript do autor Kyle Simpson
+
+## Biblioteca para pegar um input direto no console:
+
+```
+    npm install prompt-sync
+```
---
+++ functions.js
@@ -0,0 +1,26 @@
+//Exercicios básicos sobre funções
+//Parte 1 Funções simples para exibir o input no console
+function printAmount(amt) {
+    console.log(amt.toFixed(2))
+}
+
+function formatAmount(amt) {
+    console.log(`$${amt.toFixed(2)}`)
+}
+
+const prompt = require("prompt-sync")();
+
+let input = prompt('Digite um valor: ')
+
+printAmount(Number(input));
+formatAmount(Number(input));
+
+//Parte 2 Função de calculo de juros
+let Taxa = 0.05;
+
+function calculoJuros(inputt) {
+    let ValorComTaxa = Number(inputt) + Number(inputt) * Taxa
+    console.log(`Valor com juros: ${ValorComTaxa.toFixed(2)}`)
+}
+
+calculoJuros(input)
